b97f19de8a8c3bddf40cc6302f99234ded4a56a6
Vera Kosteva <kosteva.vera@gmail.com>
2016-04-06 22:23:03 +0300

    3-4

---
+++ JSHomeWork3-4/js/script.js
@@ -0,0 +1,10 @@
+/*CREATE OBJECT*/
+
+var testObj = {
+    name: document.createElement('h1'),
+    title: document.createElement('p'),
+    questions: document.createElement('p'),
+    answer: document.createElement('p'),
+    buttonSubmit: document.createElement('input'),
+},
+    
