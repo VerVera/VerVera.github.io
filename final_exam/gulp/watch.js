6b1b018245294e9a5ef9caaec411fc4668da1415
Vera <kosteva.vera@gmail.com>
2016-05-25 01:41:51 +0700

    added Masonry

--- final_exam/gulp/watch.js
+++ final_exam/gulp/watch.js
@@ -13,11 +13,10 @@ module.exports = function(gulp, plugins, args, config, taskTarget, browserSync) 
         path.join(dirs.source, dirs.styles, '**/*.{scss,sass}')
       ], ['sass']);
 
-      // HTML
-//      gulp.watch([
-//        path.join(dirs.source, '**/*.html'),
-//        path.join(dirs.source, dirs.data, '**/*.{html,htm}')
-//      ], ['html']);
+      //scripts
+      gulp.watch([
+        path.join(dirs.source, dirs.scripts, '**/*.js')
+      ], ['script']);
 
       // Copy
       gulp.watch([
@@ -33,7 +32,7 @@ module.exports = function(gulp, plugins, args, config, taskTarget, browserSync) 
       // All other files
       gulp.watch([
         path.join(dirs.temporary, '**/*'),
-        '!' + path.join(dirs.temporary, '**/*.{css,map,html,js}')
+        '!' + path.join(dirs.temporary, '**/*.{css,map,html}')
       ]).on('change', browserSync.reload);
     }
   });
---
+++ final_exam/src/_scripts/img-grid.js
@@ -0,0 +1,28 @@
+$(function () {
+    var $grid = $('.ideas_grid');
+    var w = $grid.width();
+    console.log(w);
+    $grid.masonry({
+        // options
+        itemSelector: '.ideas__item',
+        //columnWidth: 320
+        columnWidth: w / 3
+    });
+
+    $(window).resize(function () {
+        var newW = $grid.width();
+        if (newW == w) {
+            return
+        }
+        var columnWidth = 300;
+        if (newW>=768){
+            columnWidth = newW / 3
+        }
+         
+        $grid.masonry('option', {columnWidth: columnWidth});
+        w = newW;
+        
+
+    });
+
+});
