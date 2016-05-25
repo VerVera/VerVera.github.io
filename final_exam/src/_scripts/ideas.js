22b7bca502aded402611e165f9809d23d16e6b02
Vera <kosteva.vera@gmail.com>
2016-05-25 18:07:28 +0700

    added random size

--- final_exam/src/_scripts/ideas.js
+++ final_exam/src/_scripts/ideas.js
@@ -36,9 +36,19 @@ $(function () {
             r: +Math.random()
         }, function (data) {
             if (data.status == "success" && data.count > 0) {
-                for (var i = 0; i < data.count; i++) {
-                    $('.ideas__item--' + (i + 1)).find('img').attr('src', data.images[i].imageurl)
+
+                $('.ideas__item').each(function (i, item) {
+                    $(item).find('img').attr('src', data.images[i].imageurl);
+                    $(item).find('figcaption').text(data.images[i].word);
+                    if (Math.round(Math.random())) {
+                        $(item).toggleClass('ideas__item--big')
+                    }
+                });
+                if (!$("html").hasClass("ie8")) {
+                    var $grid = $('.ideas_grid');
+                    $grid.masonry();
                 }
+
             }
         }, 'json');
     }
