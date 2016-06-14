9699da715d14081fb6c31e2814de18c4f09e6d41
Vera Kosteva <kosteva.vera@gmail.com>
2016-06-14 16:58:19 +0300

    move to root

---
+++ js/script.js
@@ -0,0 +1,33 @@
+/**
+ * Created by koste on 01.06.2016.
+ */
+
+//NAV FIXED
+var h_hght = 400;
+var h_mrg = 0;
+
+$(function(){
+
+    var elem = $('#top_nav');
+    var top = $(this).scrollTop();
+
+    if(top > h_hght){
+        elem.css('top', h_mrg);
+    }
+
+    $(window).scroll(function(){
+        top = $(this).scrollTop();
+
+        if (top+h_mrg < h_hght) {
+            elem.css('top', (h_hght-top));
+        } else {
+            elem.css('top', h_mrg);
+        }
+    });
+    $("#bs-example-navbar-collapse-2").on("click", "a", function(event){
+        $('#bs-example-navbar-collapse-2').collapse('hide');
+       // event.preventDefault();
+    });
+
+});
+
